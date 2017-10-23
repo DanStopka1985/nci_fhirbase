55e7f6d11bce706575bfbf4e8ba06d8465cea244
stepan.danilov@rtlabs.ru <stepan.danilov@rtlabs.ru>
2017-10-23 17:23:35 +0300

    [first]

---
+++ main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"strconv"
+	_ "github.com/lib/pq"
+	ss "../settings"
+	rep "../dao"
+)
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/nci", rep.GetResourceSearchResult)
+	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(ss.GetSettings().Port), router))
+}
